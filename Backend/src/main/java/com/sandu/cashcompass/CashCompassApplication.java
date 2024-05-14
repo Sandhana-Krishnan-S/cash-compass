0413321bcceb2c68ac156c822ef5d400f12a5eda
Sandhana-Krishnan-S <sandhanakrishnansakthivel@gmail.com>
2024-05-14 19:11:01 +0530

    initial commit

---
+++ Backend/src/main/java/com/sandu/cashcompass/CashCompassApplication.java
@@ -0,0 +1,13 @@
+package com.sandu.cashcompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashCompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashCompassApplication.class, args);
+	}
+
+}
---
+++ Frontend/android/app/src/main/kotlin/com/sandu/cash_compass/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sandu.cash_compass
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
